897f55a2a202ffb31e379d6be83a79bed331c1c4
Terence Scott <scott.terence7@gmail.com>
2025-03-14 20:32:58 -0400

    added another database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,11 +13,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 
 import os
+from dotenv import load_dotenv
 
 from django.conf.global_settings import MEDIA_ROOT, CSRF_TRUSTED_ORIGINS, STATIC_ROOT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load environment variables from .env file
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -81,11 +85,23 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
+"""    
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD_TS'),
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432',
     }
 }
 
