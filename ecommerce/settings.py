30e8ff96906acf7cd338f4564973ec8852852924
Terence Scott <scott.terence7@gmail.com>
2025-03-14 21:03:59 -0400

    changed back to sqlite

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -83,13 +83,14 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-"""    
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 """
 
 DATABASES = {
@@ -102,6 +103,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
 
 
 # Password validation
